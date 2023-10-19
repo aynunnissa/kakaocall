91c56401c45c2c73d3d59da6fbe6353d3a260b05
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2023-10-19 20:01:34 +0700

    feat: add contact provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import client from '@/lib/client';
+import { ContactProvider } from '@/store/context/contact-context';
 import '@/styles/globals.css';
 import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <ContactProvider>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </ContactProvider>
   );
 }
