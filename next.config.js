13b79a6ac7f64bfad1135a93f39e3f2742241735
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2023-10-19 16:48:02 +0700

    chore: add manifest.json

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
+/** @type {import('next').NextConfig} */
+const runtimeCaching = require('next-pwa/cache');
+
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  runtimeCaching,
 });
-
-/** @type {import('next').NextConfig} */
 
 const nextConfig = withPWA({
   reactStrictMode: true,
