614c8e88aba50ba64bf5f85041de52e4873d9102
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2023-10-20 14:12:19 +0700

    feat: implement search contact on the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,17 @@ import Skeleton from '@/components/shared/Skeleton';
 import ContactItem from '@/components/contact/ContactItem';
 import { IContact } from '@/store/types/contact';
 import Link from 'next/link';
+import SearchContact from '@/components/contact/SearchContact';
 
-const SkeletonContainer = css({
+const verticalStack = css({
   display: 'flex',
   flexDirection: 'column',
+  gap: theme.spacing.sm,
+});
+
+const horizontalStack = css({
+  display: 'flex',
+  alignItems: 'center',
   gap: theme.spacing.sm,
 });
 
@@ -31,6 +38,8 @@ const contactListContainer = css(containerStyle, {
   boxShadow: theme.shadow.sm,
 });
 
+const FlexContainer = css(horizontalStack, contactListContainer);
+
 const subTitleText = css({
   color: theme.palette.primary.main,
   fontWeight: 700,
@@ -38,16 +47,34 @@ const subTitleText = css({
   margin: `${theme.spacing.md} 0`,
 });
 
-const contactListStyle = css({
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '1rem',
+const notFoundText = css({
+  textAlign: 'center',
 });
 
 const ContactPage = () => {
   const { state } = useContact();
   const [favoriteContacts, setFavoriteContacts] = useState<IContact[]>([]);
   const [regularContacts, setRegularContacts] = useState<IContact[]>([]);
+  const [onSearchMode, setOnSearchMode] = useState(false);
+  const [searchResult, setSearchResult] = useState<IContact[]>([]);
+
+  /**
+   * Function to handle contact search
+   */
+  const handleSearch = (query: string) => {
+    if (!query) {
+      setOnSearchMode(false);
+    } else {
+      setOnSearchMode(true);
+
+      const contactFiltered = state.contactList.filter(contact => {
+        const name = `${contact.first_name} ${contact.last_name}`;
+        if (name.toLowerCase().includes(query.toLowerCase())) return contact;
+      });
+
+      setSearchResult(contactFiltered);
+    }
+  };
 
   useEffect(() => {
     const favoriteList: IContact[] = [];
@@ -66,14 +93,17 @@ const ContactPage = () => {
     <main>
       <div css={containerStyle}>
         <h1>Phone Book</h1>
+      </div>
+      <div css={FlexContainer}>
+        <SearchContact onSearch={handleSearch} />
         <Link href="/add-contact">Add</Link>
       </div>
-      {favoriteContacts.length > 0 && (
+      {!onSearchMode && favoriteContacts.length > 0 && (
         <div css={contactListContainer}>
           <h2 css={subTitleText}>Favorite</h2>
-          <div css={contactListStyle}>
+          <div css={verticalStack}>
             {state.isLoadingContact ? (
-              <div css={SkeletonContainer}>
+              <div css={verticalStack}>
                 {[...Array(2)].map((num, ind) => (
                   <Skeleton
                     key={`skeleton-${ind}`}
@@ -96,32 +126,57 @@ const ContactPage = () => {
           </div>
         </div>
       )}
-      <div css={contactListContainer}>
-        <h2 css={subTitleText}>Contact List</h2>
-        <div css={contactListStyle}>
-          {state.isLoadingContact ? (
-            <div css={SkeletonContainer}>
-              {[...Array(3)].map((num, ind) => (
-                <Skeleton
-                  key={`skeleton-${ind}`}
-                  customClass={{ height: '50px', width: '100%' }}
+      {!onSearchMode && (
+        <div css={contactListContainer}>
+          <h2 css={subTitleText}>Contact List</h2>
+          <div css={verticalStack}>
+            {state.isLoadingContact ? (
+              <div css={verticalStack}>
+                {[...Array(3)].map((num, ind) => (
+                  <Skeleton
+                    key={`skeleton-${ind}`}
+                    customClass={{ height: '50px', width: '100%' }}
+                  />
+                ))}
+              </div>
+            ) : (
+              regularContacts.map((item, ind: number) => (
+                <ContactItem
+                  key={`contact-${ind}`}
+                  id={item.id}
+                  first_name={item.first_name}
+                  last_name={item.last_name}
+                  phones={item.phones}
+                  is_favorite={item.is_favorite}
                 />
-              ))}
+              ))
+            )}
+          </div>
+        </div>
+      )}
+      {onSearchMode && (
+        <div css={contactListContainer}>
+          {searchResult.length > 0 ? (
+            <div>
+              <h2 css={subTitleText}>Contact List</h2>
+              <div css={verticalStack}>
+                {searchResult.map((item, ind: number) => (
+                  <ContactItem
+                    key={`contact-${ind}`}
+                    id={item.id}
+                    first_name={item.first_name}
+                    last_name={item.last_name}
+                    phones={item.phones}
+                    is_favorite={item.is_favorite}
+                  />
+                ))}
+              </div>
             </div>
           ) : (
-            regularContacts.map((item, ind: number) => (
-              <ContactItem
-                key={`contact-${ind}`}
-                id={item.id}
-                first_name={item.first_name}
-                last_name={item.last_name}
-                phones={item.phones}
-                is_favorite={item.is_favorite}
-              />
-            ))
+            <p css={notFoundText}>Contact not found</p>
           )}
         </div>
-      </div>
+      )}
     </main>
   );
 };
