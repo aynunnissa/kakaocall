046557487ee96bc1580bfd138d9fcc68595bb9ac
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2023-10-21 11:51:25 +0700

    style: add responsive style to header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,12 +12,10 @@ import { IContact } from '@/store/types/contact';
 import Link from 'next/link';
 import SearchContact from '@/components/contact/SearchContact';
 import Pagination from '@/components/shared/Pagination';
+import Header from '@/components/layout/Header';
+import MainContainer from '@/components/layout/MainContainer';
 
-const containerStyle = css({
-  padding: `${theme.spacing.lg} ${theme.spacing.xl}`,
-});
-
-const contactListContainer = css(containerStyle, {
+const contactListContainer = css({
   margin: `${theme.spacing.lg} 0`,
   padding: `${theme.spacing.sm} ${theme.spacing.lg}`,
   borderRadius: theme.shape.rounded.xl,
@@ -26,22 +24,58 @@ const contactListContainer = css(containerStyle, {
 
 const searchBox = css(contactListContainer, {
   padding: `${theme.spacing.sm} ${theme.spacing.md}`,
+  marginTop: 0,
+
+  [theme.breakpoints.md]: {
+    display: 'none',
+  },
 });
 
-const headerStyle = css({
+const mobileSearchBox = css({
+  display: 'none',
+
+  [theme.breakpoints.md]: {
+    display: 'block',
+    width: '300px',
+  },
+
+  [theme.breakpoints.lg]: {
+    display: 'block',
+    width: '450px',
+  },
+});
+
+const addLinkStyle = css({
+  textDecoration: 'none',
   display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  gap: theme.spacing.sm,
-  color: theme.palette.primary.main,
-});
-
-const titleText = css({
   fontSize: theme.text.xxl,
 
   [theme.breakpoints.md]: {
-    fontSize: theme.text['2xl'],
+    display: 'flex',
+    alignItems: 'center',
+    gap: theme.spacing.sm,
+    fontSize: theme.text.xl,
+    color: theme.palette.primary.main,
+    padding: theme.spacing.sm,
+    textDecoration: 'none',
+    backgroundColor: theme.palette.primary.light,
+    borderRadius: theme.shape.rounded.lg,
   },
+});
+
+const addTextStyle = css({
+  display: 'none',
+  fontSize: theme.text.md,
+  fontWeight: 600,
+
+  [theme.breakpoints.md]: {
+    display: 'inline',
+  },
+});
+
+const headerRightSide = css({
+  display: 'flex',
+  gap: theme.spacing.md,
 });
 
 const verticalStack = css({
@@ -51,14 +85,6 @@ const verticalStack = css({
 
   [theme.breakpoints.md]: {
     gap: theme.spacing.xl,
-  },
-});
-
-const linkItem = css({
-  textDecoration: 'none',
-
-  '> span': {
-    fontSize: theme.text.xxl,
   },
 });
 
@@ -74,7 +100,14 @@ const subTitleText = css({
 });
 
 const notFoundText = css({
+  fontSize: theme.text.md,
   textAlign: 'center',
+});
+
+const contactFoundText = css({
+  fontSize: theme.text.sm,
+  color: theme.palette.grey[400],
+  fontWeight: 400,
 });
 
 /**
@@ -104,17 +137,19 @@ const gridContainer = css({
 const absolutePagination = css({
   position: 'absolute',
   bottom: 0,
-  right: theme.spacing.lg,
+  right: theme.spacing.xl,
 });
 
 const ContactPage = () => {
   const { state } = useContact();
+  const [isLoadingPage, setIsLoadingPage] = useState(true);
   const [favoriteContacts, setFavoriteContacts] = useState<IContact[]>([]);
   const [regularContacts, setRegularContacts] = useState<IContact[]>([]);
   const [visibleContacts, setVisibleContacts] = useState<IContact[]>([]);
   const [onSearchMode, setOnSearchMode] = useState(false);
   const [searchResult, setSearchResult] = useState<IContact[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
 
   const handlePageChanged = (value: number) => {
     setCurrentPage(value);
@@ -147,9 +182,22 @@ const ContactPage = () => {
       else regularList.push(contact);
     });
 
+    const totalPageCount = Math.ceil(regularList.length / 10);
+
     setFavoriteContacts(favoriteList);
     setRegularContacts(regularList);
+    setTotalPage(totalPageCount);
   }, [state.contactList]);
+
+  /**
+   * Effect for handling page count changes because the regular contact list count changes
+   * due to a favorite or delete action
+   */
+  useEffect(() => {
+    if (currentPage > totalPage && totalPage > 0) {
+      setCurrentPage(totalPage);
+    }
+  }, [currentPage, totalPage]);
 
   useEffect(() => {
     if (currentPage < 1) return;
@@ -158,88 +206,42 @@ const ContactPage = () => {
     const endIndex = currentPage * 10;
 
     setVisibleContacts(regularContacts.slice(startIndex, endIndex));
+    setIsLoadingPage(false);
   }, [currentPage, regularContacts]);
 
   return (
-    <main css={containerStyle}>
-      <div css={headerStyle}>
-        <h1 css={titleText}>Phone Book</h1>
-        <Link href="/add-contact" css={linkItem}>
-          <span className="kao-person_add_alt"></span>
-        </Link>
-      </div>
-      <div css={searchBox}>
-        <SearchContact onSearch={handleSearch} />
-      </div>
-      {!onSearchMode && favoriteContacts.length > 0 && (
-        <div css={contactListContainer}>
-          <h2 css={subTitleText}>Favorite</h2>
-          <div css={verticalStack}>
-            {state.isLoadingContact ? (
-              <div css={verticalStack}>
-                {[...Array(2)].map((num, ind) => (
-                  <Skeleton
-                    key={`skeleton-${ind}`}
-                    customClass={{ height: '50px', width: '100%' }}
-                  />
-                ))}
-              </div>
-            ) : (
-              favoriteContacts.map((item, ind: number) => (
-                <ContactItem
-                  key={`contact-${ind}`}
-                  id={item.id}
-                  first_name={item.first_name}
-                  last_name={item.last_name}
-                  phones={item.phones}
-                  is_favorite={item.is_favorite}
-                />
-              ))
-            )}
+    <div>
+      <Header justify="space-between">
+        <h1>Phone Book</h1>
+        <div css={headerRightSide}>
+          <div css={mobileSearchBox}>
+            <SearchContact onSearch={handleSearch} />
           </div>
+          <Link href="/add-contact" css={addLinkStyle}>
+            <span className="kao-person_add_alt"></span>
+            <span css={addTextStyle}>Add contact</span>
+          </Link>
         </div>
-      )}
-      {!onSearchMode && (
-        <div css={contactListContainer}>
-          <h2 css={subTitleText}>Contact List</h2>
-          <div css={gridContainer}>
-            <div></div>
-            <p>Name</p>
-            <p>Phone Number</p>
-            <div></div>
-          </div>
-          <div css={verticalStack}>
-            {state.isLoadingContact ? (
-              <div css={verticalStack}>
-                {[...Array(3)].map((num, ind) => (
-                  <Skeleton
-                    key={`skeleton-${ind}`}
-                    customClass={{ height: '50px', width: '100%' }}
-                  />
-                ))}
-              </div>
-            ) : (
-              visibleContacts.map((item, ind: number) => (
-                <ContactItem
-                  key={`contact-${ind}`}
-                  id={item.id}
-                  first_name={item.first_name}
-                  last_name={item.last_name}
-                  phones={item.phones}
-                  is_favorite={item.is_favorite}
-                />
-              ))
-            )}
-          </div>
+      </Header>
+      <MainContainer>
+        <div css={searchBox}>
+          <SearchContact onSearch={handleSearch} />
         </div>
-      )}
-      {onSearchMode && (
-        <div css={contactListContainer}>
-          {searchResult.length > 0 ? (
-            <div>
-              <h2 css={subTitleText}>Contact List</h2>
-              <div css={verticalStack}>
-                {searchResult.map((item, ind: number) => (
+        {!onSearchMode && favoriteContacts.length > 0 && (
+          <div css={contactListContainer}>
+            <h2 css={subTitleText}>Favorite</h2>
+            <div css={verticalStack}>
+              {state.isLoadingContact ? (
+                <div css={verticalStack}>
+                  {[...Array(3)].map((num, ind) => (
+                    <Skeleton
+                      key={`skeleton-${ind}`}
+                      customClass={{ height: '50px', width: '100%' }}
+                    />
+                  ))}
+                </div>
+              ) : (
+                favoriteContacts.map((item, ind: number) => (
                   <ContactItem
                     key={`contact-${ind}`}
                     id={item.id}
@@ -248,29 +250,90 @@ const ContactPage = () => {
                     phones={item.phones}
                     is_favorite={item.is_favorite}
                   />
-                ))}
-              </div>
+                ))
+              )}
             </div>
-          ) : (
-            <p css={notFoundText}>Contact not found</p>
-          )}
-        </div>
-      )}
-      {Math.ceil(regularContacts.length / 10) > 1 && (
-        <div
-          css={
-            visibleContacts.length + favoriteContacts.length < 10 &&
-            absolutePagination
-          }
-        >
-          <Pagination
-            totalPages={Math.ceil(regularContacts.length / 10)}
-            currentPage={currentPage}
-            pageChanged={handlePageChanged}
-          />
-        </div>
-      )}
-    </main>
+          </div>
+        )}
+        {!onSearchMode && (
+          <div css={contactListContainer}>
+            <h2 css={subTitleText}>Contact List</h2>
+            <div css={gridContainer}>
+              <div></div>
+              <p>Name</p>
+              <p>Phone Number</p>
+              <div></div>
+            </div>
+            <div css={verticalStack}>
+              {state.isLoadingContact || isLoadingPage ? (
+                <div css={verticalStack}>
+                  {[...Array(5)].map((num, ind) => (
+                    <Skeleton
+                      key={`skeleton-${ind}`}
+                      customClass={{ height: '50px', width: '100%' }}
+                    />
+                  ))}
+                </div>
+              ) : (
+                visibleContacts.map((item, ind: number) => (
+                  <ContactItem
+                    key={`contact-${ind}`}
+                    id={item.id}
+                    first_name={item.first_name}
+                    last_name={item.last_name}
+                    phones={item.phones}
+                    is_favorite={item.is_favorite}
+                  />
+                ))
+              )}
+            </div>
+          </div>
+        )}
+        {onSearchMode && (
+          <div css={contactListContainer}>
+            {searchResult.length > 0 ? (
+              <div>
+                <h2 css={subTitleText}>
+                  Contact List{' '}
+                  <span css={contactFoundText}>
+                    ({searchResult.length} found)
+                  </span>
+                </h2>
+
+                <div css={verticalStack}>
+                  {searchResult.map((item, ind: number) => (
+                    <ContactItem
+                      key={`contact-${ind}`}
+                      id={item.id}
+                      first_name={item.first_name}
+                      last_name={item.last_name}
+                      phones={item.phones}
+                      is_favorite={item.is_favorite}
+                    />
+                  ))}
+                </div>
+              </div>
+            ) : (
+              <p css={notFoundText}>Contact not found</p>
+            )}
+          </div>
+        )}
+        {totalPage > 1 && !onSearchMode && (
+          <div
+            css={
+              visibleContacts.length + favoriteContacts.length < 10 &&
+              absolutePagination
+            }
+          >
+            <Pagination
+              totalPages={totalPage}
+              currentPage={currentPage}
+              pageChanged={handlePageChanged}
+            />
+          </div>
+        )}
+      </MainContainer>
+    </div>
   );
 };
 
